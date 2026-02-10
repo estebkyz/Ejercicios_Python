34d2bf3e5e0af2dece636b366e2395218f4c6421
jeronimo777-AdS <jeronimocadavid15@gmail.com>
2026-02-09 22:12:12 -0500

    si

--- ejercicio_22.py
+++ ejercicio_22.py
@@ -1,4 +1,4 @@
-print("Ingrese la capacidad de cada balde en litros:\n")
+print("Digite la capacidad de cada balde en litros:\n")
 balde1 = float(input("Balde 1: "))
 balde2 = float(input("Balde 2: "))
 balde3 = float(input("Balde 3: "))
@@ -9,7 +9,6 @@ tiempo1 = balde1 * tiempo_por_litro
 tiempo2 = balde2 * tiempo_por_litro
 tiempo3 = balde3 * tiempo_por_litro
 
-# Mostramos los resultados
 print("\nTiempos de llenado:")
 print("Balde 1 (", balde1, "litros):", tiempo1, "horas")
 print("Balde 2 (", balde2, "litros):", tiempo2, "horas")
